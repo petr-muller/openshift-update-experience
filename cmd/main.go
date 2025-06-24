9a38e56e7f93f1a22518009d5b64896bfbada72a
Petr Muller <muller@redhat.com>
2025-06-24 15:10:51 +0200

    build(ClusterOperatorProgressInsight): `operator-sdk create api`
    ```console
    $ operator-sdk create api --controller=true --resource=true --group openshift --version v1alpha1 --kind ClusterOperatorProgressInsight --namespaced=false
    INFO[0000] Writing kustomize manifests for you to edit...
    INFO[0000] Writing scaffold for you to edit...
    INFO[0000] api/v1alpha1/clusteroperatorprogressinsight_types.go
    INFO[0000] api/v1alpha1/groupversion_info.go
    INFO[0000] internal/controller/suite_test.go
    INFO[0000] internal/controller/clusteroperatorprogressinsight_controller.go
    INFO[0000] internal/controller/clusteroperatorprogressinsight_controller_test.go
    INFO[0000] Update dependencies:
    go mod tidy
    INFO[0000] Running make:
    make generate
    /home/pmuller/Projects/RH/github.com/petr-muller/openshift-update-experience/bin/controller-gen object:headerFile="hack/boilerplate.go.txt" paths="./..."
    Next: implement your new API and generate the manifests (e.g. CRDs,CRs) with:
    make manifests
    ```

--- cmd/main.go
+++ cmd/main.go
@@ -208,6 +208,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "ClusterVersionProgressInsight")
 		os.Exit(1)
 	}
+	if err = (&controller.ClusterOperatorProgressInsightReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ClusterOperatorProgressInsight")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if metricsCertWatcher != nil {
